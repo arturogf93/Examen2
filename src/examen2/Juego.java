cc998611761824f32b47118dc4d5f5284a4acb11
arturogf93 <arturo1_g@hotmail.com>
2014-03-11 19:02:31 -0600

    first

--- src/examen2/Juego.java
+++ src/examen2/Juego.java
@@ -22,6 +22,10 @@ import java.net.URL;
 import java.util.LinkedList;
 import java.awt.Font;
 import java.awt.Rectangle;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Juego extends JFrame implements Runnable, KeyListener, MouseListener, MouseMotionListener {
 
@@ -46,10 +50,16 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
     private Image pierdes;
     private Image playAgain;
 
+    private SoundClip fly;
+    private SoundClip choque;
+    private SoundClip bing;
+
     private int vy;
     private int gravedad;
     private int avance;
     private int contgravedad;
+    private int hScore;
+    private int nivel;
 
     private boolean pausa;              //Booleando para pausa
     private boolean inicio;
@@ -58,6 +68,17 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
     private boolean nivel1;
     private boolean nivel2;
     private boolean nivel3;
+    private boolean choquesonido;
+    private boolean guarda;             //flag para saber si se guarda
+    private boolean carga;
+    private boolean newScore;
+    private boolean nivelMensaje;
+
+    private File archivo;               //File para el archivo a crear
+    private FileReader fr;              //fileReader para la lectura del archivo
+    private BufferedReader br;
+    private FileWriter file;            //un FileWriter para el manejo de archivos
+    private PrintWriter out;
 
     private LinkedList<BaseEnemigo> tubosA;
     private LinkedList<BaseEnemigo> tubosB;
@@ -67,13 +88,15 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
      * En este metodo se inizializan las variables o se crean los objetos a
      * usarse en el <code>Applet</code> y se definen funcionalidades.
      */
-    public void init() {
+    public void init() throws IOException {
         nivel1 = true;
         nivel2 = false;
         nivel3 = false;
         juega = false;
         contgravedad = 1;
         gravedad = 1;
+        hScore = 0;
+        nivel = 1;
         this.setSize(286, 510);
         addKeyListener(this);           //Uso de las teclas
         addMouseListener(this);          //Uso de las teclas
@@ -81,6 +104,11 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
         inicio = true;
         pausa = false;
         gameover = false;
+        choquesonido = false;
+        guarda = false;
+        carga = false;
+        newScore = false;
+        nivelMensaje = false;
         fondo1 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Images/Background1.png"));
         titulo = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Images/Titulo.png"));
         tuboA = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Images/TuboArriba.png"));
@@ -89,26 +117,34 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
         getReady = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Images/GetReady.png"));
         pierdes = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Images/GameOver.png"));
         playAgain = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Images/PlayAgain.png"));
+        fly = new SoundClip("sounds/fly.wav");
+        choque = new SoundClip("sounds/choque.wav");
+        bing = new SoundClip("sounds/bing.wav");
         piso = new Piso(0, this.getHeight() - 112);
         piso.setPosY(this.getHeight() - piso.getHeight());
         tubosA = new LinkedList();
         tubosB = new LinkedList();
+
+        file = new FileWriter("hola.txt");
+        out = new PrintWriter(file);
+        archivo = new File("hola.txt");
+        fr = new FileReader(archivo);
+        br = new BufferedReader(fr);
+
         for (int i = 1; i <= 3; i++) {
             int yfinal = 0;
             int y = 0;
-            if (nivel1){
-                    y = ((int) (Math.random() * 100)) + 155;
-                    yfinal =this.getHeight() - y - 485;
-                }
-                else if (nivel2){
-                    y = ((int) (Math.random() * 120)) + 155;
-                    yfinal =this.getHeight() - y - 460;
-                }
-                else if (nivel3){
-                    y = ((int) (Math.random() * 140)) + 155;
-                    yfinal =this.getHeight() - y - 425;
-                }
-            
+            if (nivel1) {
+                y = ((int) (Math.random() * 100)) + 155;
+                yfinal = this.getHeight() - y - 485;
+            } else if (nivel2) {
+                y = ((int) (Math.random() * 120)) + 155;
+                yfinal = this.getHeight() - y - 460;
+            } else if (nivel3) {
+                y = ((int) (Math.random() * 140)) + 155;
+                yfinal = this.getHeight() - y - 425;
+            }
+
             tubosA.add(new BaseEnemigo(this.getWidth() + (160 * i), yfinal, tuboA));
             tubosB.add(new BaseEnemigo(this.getWidth() + (160 * i), this.getHeight() - y, tuboB));
         }
@@ -135,7 +171,11 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
     public void run() {
         while (true) {
             if (!pausa) {
-                actualiza();
+                try {
+                    actualiza();
+                } catch (IOException ex) {
+                    Logger.getLogger(Juego.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 checaColision();
             }
             repaint();    // Se actualiza el <code>Applet</code> repintando el contenido.
@@ -152,7 +192,7 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
      * Metodo usado para actualizar la posicion de objetos elefante y raton.
      *
      */
-    public void actualiza() {
+    public void actualiza() throws IOException {
         //if(movimiento){
         long tiempoTranscurrido = System.currentTimeMillis() - tiempoActual;
 
@@ -166,8 +206,8 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
             (flappy.getImagenes()).actualiza(tiempoActual);
 
             if (juega) {
-                avance = (vy + (gravedad * (contgravedad/3)));
-                contgravedad+=2;
+                avance = (vy + (gravedad * (contgravedad / 3)));
+                contgravedad += 2;
                 if (avance > 8) {
                     avance = 8;
                 }
@@ -181,10 +221,11 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
                     BaseEnemigo actualB = (BaseEnemigo) (tubosB.get(i));
                     actualA.setPosX(actualA.getPosX() - 2);
                     actualB.setPosX(actualA.getPosX() - 2);
-                    if (actualA.getPosX()== flappy.getPosX()+1){
-                        flappy.setScore(flappy.getScore()+1);
+                    if (actualA.getPosX() == flappy.getPosX() + 1) {
+                        flappy.setScore(flappy.getScore() + 1);
+                        bing.play();
                     }
-                    
+
                 }
             }
             if (flappy.getScore() > 8 && flappy.getScore() <= 18) {
@@ -197,6 +238,48 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
                 nivel2 = false;
                 nivel3 = true;
             }
+
+            if (flappy.getScore() >= 0 && flappy.getScore() <= 2 && nivel1) {
+                nivel = 1;
+                nivelMensaje = true;
+            } else if (flappy.getScore() > 2 && nivel1){
+                nivelMensaje = false;
+            }
+            
+            if (flappy.getScore() > 9 && flappy.getScore() <= 11 && nivel2) {
+                nivel = 2;
+                nivelMensaje = true;
+            } else if (flappy.getScore() > 10 && nivel2){
+                nivelMensaje = false;
+            }
+
+            if (flappy.getScore() > 18 && flappy.getScore() <= 20 && nivel3) {
+                nivel = 3;
+                nivelMensaje = true;
+            } else if (flappy.getScore() > 20 && nivel3){
+                nivelMensaje = false;
+            }
+        }
+
+        if (guarda) {
+            archivo = new File("guarda.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+            String linea;
+            linea = br.readLine();
+            System.out.println(linea);
+            int highscore = Integer.parseInt(linea);
+            if (highscore < flappy.getScore()) {
+                file = new FileWriter("guarda.txt");
+                out = new PrintWriter(file);
+                out.println(flappy.getScore());
+                hScore = flappy.getScore();
+                newScore = true;
+            } else {
+                hScore = highscore;
+            }
+            file.close();
+            guarda = false;
         }
 
     }
@@ -211,15 +294,13 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
             BaseEnemigo actualB = (BaseEnemigo) (tubosB.get(i));
             if (actualA.getPosX() + actualA.getWidth() < 0) {
                 int y = 0;
-                if (nivel1){
+                if (nivel1) {
                     y = ((int) (Math.random() * 100)) + 155;
                     actualA.setPosY(this.getHeight() - y - 485);
-                }
-                else if (nivel2){
+                } else if (nivel2) {
                     y = ((int) (Math.random() * 120)) + 155;
                     actualA.setPosY(this.getHeight() - y - 460);
-                }
-                else if (nivel3){
+                } else if (nivel3) {
                     y = ((int) (Math.random() * 140)) + 155;
                     actualA.setPosY(this.getHeight() - y - 425);
                 }
@@ -227,16 +308,22 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
                 actualA.setPosX(446);
                 actualB.setPosX(446);
             }
-            if (actualA.intersecta(flappy) || actualB.intersecta(flappy)) {
+            if ((actualA.intersecta(flappy) || actualB.intersecta(flappy)) && !choquesonido) {
                 juega = false;
                 gameover = true;
                 pausa = false;
                 inicio = false;
+                choque.play();
+                choquesonido = true;
+                guarda = true;
             }
         }
-        if (flappy.intersecta(piso)) {
+        if (flappy.intersecta(piso) && !choquesonido) {
             juega = false;
             gameover = true;
+            choque.play();
+            choquesonido = true;
+            guarda = true;
         }
 
     }
@@ -316,6 +403,12 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
             }
             contgravedad = 0;
             vy = -7;
+            fly.play();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_G) {  //dejo de presionar la tecla de arriba
+            if (!guarda) {
+                guarda = true;
+            }
         }
 
     }
@@ -329,10 +422,11 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
      * @param g es el <code>objeto grafico</code> usado para dibujar.
      */
     public void paint1(Graphics g) {
-        
+        g.setFont(new Font("TimesRoman", Font.BOLD, 30));
+        g.setColor(Color.WHITE);
         if (piso != null) {
             g.drawImage(fondo1, 0, 0, this);
-            
+
             for (int i = 0; i < tubosA.size(); i++) {
                 BaseEnemigo actualA = (BaseEnemigo) (tubosA.get(i));
                 BaseEnemigo actualB = (BaseEnemigo) (tubosB.get(i));
@@ -348,8 +442,16 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
             if (gameover) {
                 g.drawImage(pierdes, this.getWidth() / 2 - pierdes.getWidth(this) / 2, this.getHeight() / 2 - 130, this);
                 g.drawImage(playAgain, this.getWidth() / 2 - playAgain.getWidth(this) / 2, this.getHeight() / 2, this);
+                if (newScore) {
+                    g.drawString("New Highscore " + hScore, 30, 230);
+                } else {
+                    g.drawString("Highscore " + hScore, 55, 230);
+                }
             }
-            g.drawString(""+ flappy.getScore(), 50, 50);
+            if (nivelMensaje) {
+                g.drawString("Nivel " + nivel, 10, 500);
+            }
+            g.drawString("" + flappy.getScore(), this.getWidth() / 2, 50);
         } else {
             //Da un mensaje mientras se carga el dibujo	
             g.drawString("No se cargo la imagen..", 20, 20);
@@ -367,9 +469,9 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
             juega = false;
             pausa = false;
             inicio = true;
-            nivel1=true;
-            nivel2=false;
-            nivel3=false;
+            nivel1 = true;
+            nivel2 = false;
+            nivel3 = false;
             flappy.setScore(0);
             int tam = tubosA.size();
             tubosA.clear();
@@ -379,6 +481,8 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
                 tubosA.add(new BaseEnemigo(this.getWidth() + (160 * i), this.getHeight() - y - 485, tuboA));
                 tubosB.add(new BaseEnemigo(this.getWidth() + (160 * i), this.getHeight() - y, tuboB));
             }
+            choquesonido = false;
+            newScore = false;
         }
     }
 
